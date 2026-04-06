3cda69bfa7865df1563f2fe7fd7c1e07775a32ad
GolikovDO <xaxatumbik@gmail.com>
2026-04-06 23:26:21 +0300

    MOVE DB to https://railway.com/

--- src/main/java/com/caseo/infrastructure/config/HibernateConfig.java
+++ src/main/java/com/caseo/infrastructure/config/HibernateConfig.java
@@ -60,9 +60,12 @@ public class HibernateConfig {
         Configuration config = new Configuration();
 
         config.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
-        config.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/PMLLPA");
-        config.setProperty("hibernate.connection.username", "Admin");
-        config.setProperty("hibernate.connection.password", "Dimon678");
+        config.setProperty("hibernate.connection.url", "jdbc:postgresql://maglev.proxy.rlwy.net:43448/railway");
+        config.setProperty("hibernate.connection.username", "postgres");
+        config.setProperty("hibernate.connection.password", "SWtVkqzxdUqWVpJxwnybRXxDfvJCJTna");
+//        config.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/PMLLPA");
+//        config.setProperty("hibernate.connection.username", "Admin");
+//        config.setProperty("hibernate.connection.password", "Dimon678");
 
         config.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
         config.setProperty("hibernate.show_sql", "false");
