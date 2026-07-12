71e9c175f2fa015321d8024b151a683781670610
GolikovDO <xaxatumbik@gmail.com>
2026-07-12 22:02:52 +0300

    feat(security): add Spring Security authentication infrastructure
    - added Spring Security starter
    - added SecurityConfig
    - added UserPrincipal
    - added CustomUserDetailsService
    - configured AuthenticationManager
    - configured PasswordEncoder
    - configured DaoAuthenticationProvider
    - enabled Spring Security filter chain
    - removed @ServletComponentScan

---
+++ src/main/java/ru/ecospas/infrastructure/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package ru.ecospas.infrastructure.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+import ru.ecospas.domain.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String login)
+            throws UsernameNotFoundException {
+
+        return repository.findByLogin(login)
+                .map(UserPrincipal::new)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "Пользователь '%s' не найден".formatted(login)));
+    }
+}
---
+++ src/main/java/ru/ecospas/infrastructure/security/UserPrincipal.java
@@ -0,0 +1,64 @@
+package ru.ecospas.infrastructure.security;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.ecospas.domain.model.Role;
+import ru.ecospas.domain.model.User;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class UserPrincipal implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(
+                new SimpleGrantedAuthority("ROLE_" + user.getRole().name())
+        );
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getLogin();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public Integer getId() {
+        return user.getId();
+    }
+
+    public Role getRole() {
+        return user.getRole();
+    }
+}
